42303aec24b8aa539935fcce46812bb67c00b56c
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-03-13 13:17:57 -0500

    Added capturing input

---
+++ basic_tutorials/capturing_input.py
@@ -0,0 +1,11 @@
+
+name = raw_input("What is your name? ")
+year = raw_input("What year were you born in? ")
+year = int(year)
+years_old = 2015 - year
+
+print "Hello, " + name + "!"
+print "You are " + str(years_old) + "."
+
+print "Hello, ", name, "!"
+print "You are", years_old, "."
