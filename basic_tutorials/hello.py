847ac8e40a7d376157db27f6f1b8ebc02f238a02
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-03-18 09:22:32 -0500

    Reworked with more focus on gaming

--- basic_tutorials/hello.py
+++ basic_tutorials/hello.py
@@ -1,3 +1,16 @@
-#!/usr/bin/env python
+print "Hello, 'World'"  # print string
+print 'Hello, "World"'  # exactly the same as above
 
-print "Hello, World!"  # Our first hello, world in python
+print 100  # integer number or int
+print 0.2  # floating point numbers or float
+
+print 100 * 0.2  # results in a float
+
+print 3 * 2  # multiply
+print 3 + 2  # addition
+print 3 - 2  # subtraction
+print 3 / 2  # division  results in integer and therefore truncates  1.5
+print 3.0 / 2.0
+
+print "Attack Power:", 100, "points"
+print "Percent to Hit:", 0.2 * 100, "%"
