41f11b464a6e37ed3deae600f28d6fa86f44ae7d
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-03-23 08:22:26 -0500

    String methods

---
+++ basic_tutorials/strings.py
@@ -0,0 +1,21 @@
+attack_power = 100
+
+# Print keyword
+print "Attack Power:", attack_power
+print "Attack Power: {} points".format(attack_power)
+print "Attack Power: {attack_power} points".format(attack_power=100)
+
+print "Attack Power: %s" % (attack_power)  # python 1 and 2... won't work on 3
+
+# Print as a built-in function
+print("Attack Power:", attack_power)
+print("Attack Power: {} points".format(attack_power))
+#                                       0th           1st             ...
+print("Attack Power: {0} points".format(attack_power, percent_to_hit, ...))
+print("Attack Power: {attack_power} points".format(attack_power=100))
+
+print "Attack Power".lower()  # attack power
+print "Attack Power".upper()  # ATTACK POWER
+print "Attack Power".capitalize()  # Attack power
+
+print ":".join("Attack Power", "{}".format(attack_power))  # Attack Power : 100
