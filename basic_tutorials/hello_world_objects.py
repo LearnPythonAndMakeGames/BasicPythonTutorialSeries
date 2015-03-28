75310431006950b07e066a5168b2a4dc3670c0de
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-03-28 15:02:35 -0500

    objects and Item

---
+++ basic_tutorials/hello_world_objects.py
@@ -0,0 +1,28 @@
+
+def display(message):
+    """Standard way to print a message"""
+    print message
+
+
+class HelloWorld(object):
+    """Describes your function... display a message"""
+
+    def __init__(self, message):
+        self.message = message
+
+    def display(self):
+        """Display a message"""
+        print self.message
+
+
+hello_world_object_01 = HelloWorld("hi")
+hello_world_object_02 = HelloWorld("Hello, World!")
+hello_world_object_03 = HelloWorld("Yo, sup!")
+
+hello_world_object_01.display()
+hello_world_object_02.display()
+hello_world_object_03.display()
+
+display("hi")
+display("Hello, World!")
+display("Yo, sup!")
