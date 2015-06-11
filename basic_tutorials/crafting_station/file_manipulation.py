a3cd3f4b5fd46c1b97366d4208062dadb568bb41
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-06-11 14:47:26 -0500

    pep8

--- basic_tutorials/crafting_station/file_manipulation.py
+++ basic_tutorials/crafting_station/file_manipulation.py
@@ -1,9 +1,11 @@
 import yaml
 
+
 def yaml_load(filepath):
     """Loads yaml file and returns data"""
     with open(filepath, 'r') as fd:
         return yaml.load(fd)
+
 
 def yaml_dump(filepath, data):
     """Loads yaml file and returns data"""
