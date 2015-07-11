5cd09e73a4134a6e3c135c0db43f025f7bb09094
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-07-11 17:05:51 -0500

    Coding Challenge 03

--- basic_tutorials/crafting_station/Item.py
+++ basic_tutorials/crafting_station/Item.py
@@ -1,104 +1,261 @@
-from file_manipulation import yaml_load
+#!/usr/bin/env python
+import yaml
+
+
+def yaml_load(filepath):
+    """Loads yaml file and returns data"""
+    with open(filepath, 'r') as fd:
+        return yaml.load(fd)
 
 
 class Item(object):
+    """Item Class
 
-    """Basic item"""
-
-    property_fields = dict((name, value)
-                           for name, value
-                           in yaml_load('property_data.yaml').iteritems())
+    Doctest:
+    >>> item_data = [
+    ...    ('iron hilt', {'weight': 400, 'damage': 5}),
+    ...    ('Valyrian steel', {'weight': 900, 'damage': 8}),
+    ...    ('leather strips', {'weight': 150, 'damage': 1})
+    ... ]
+    >>> items = dict((item_name, Item(name=item_name, **item_values))
+    ...               for item_name, item_values in item_data)
+    >>> item = items['iron hilt'] + items['Valyrian steel'] + 5*items['leather strips']
+    >>> item.name = "Longclaw"
+    >>> print item
+    <Item Longclaw [1450 grams]>
+    >>> print item.value
+    0.5
+    >>> print items['iron hilt'].value
+    0.497222222222
+    >>> print items['Valyrian steel'].value
+    0.504444444444
+    >>> print items['leather strips'].value
+    1.7
+    """
+    field_filepath = "item_property_data.yaml"
+    fields = dict((name, value)
+                  for name, value in yaml_load(field_filepath).iteritems())
 
     @property
-    def health(self):
-        quantity = 0
-        health_value = 0.0
-        count = len(self)
+    def value(self):
+        """Intrinsic value of item"""
+        value = 0.0
+        prop_count = len(self)
         for prop_name, prop_value in self.iteritems():
-            start, end = self.property_fields.get(prop_name, (0, 0))
-            prop_value = prop_value / count
-            if start <= prop_value <= end:
-                health_value += abs(prop_value)
-            else:
-                prop_delta = end - start
-                if start < 0:
-                    prop_delta = prop_delta / 2.0
-                shifted_value = float(prop_value) + start
-                multiplier = shifted_value / prop_delta
-                multiplied_value = float(prop_value) * multiplier
-                health_value -= abs(multiplied_value)
-        if quantity > 1:
-            health_value = health_value / self.quantity
-        return health_value
+            start, end = self.fields.get(prop_name, (0, 0))
+            if isinstance(prop_value, (int, float)):
+                prop_value = prop_value / prop_count
+                if start <= prop_value <= end:
+                    value += abs(prop_value)
+                else:
+                    delta = end - start
+                    if start < 0:
+                        delta = delta / 2.0
+                    shift_value = float(prop_value) + start
+                    multiplier = shift_value / delta
+                    multiplied_value = float(prop_value) * multiplier
+                    value += abs(multiplied_value)
+        weight = self.get('weight', 1)
+        qty = self.get('quantity', 1)
+        weight = 1 if weight == 0 else weight
+        value = (value / weight) * qty
+        return value
 
-    def __init__(self, name, quantity=1, properties=None):
-        self.name = name
-        self.quantity = quantity
-        self.properties = properties if properties is not None else {}
+    def __mul__(self, other):
+        """Increases quantity
 
-    def __getitem__(self, name):
-        """"""
-        return self.get(name)
+        >>> item = Item("mul test")
+        >>> print item.quantity
+        None
+        >>> item * 5
+        >>> print item.quantity
+        5
+        """
+        if isinstance(other, int):
+            self.quantity = other
 
-    def __setitem__(self, name, value):
-        """"""
-        if name in self.property_fields:
-            self.properties[name] = value
+    def __rmul__(self, other):
+        """Increases quantity
+
+        >>> item = Item("rmul test")
+        >>> print item.quantity
+        None
+        >>> 5 * item
+        >>> print item.quantity
+        5
+        """
+        if isinstance(other, int):
+            self.quantity = other
+
+    def __getattr__(self, key):
+        """Returns properties dictionary values as if they
+        were properties on the class.
+
+        >>> item = Item("getattr test", weight=10)
+        >>> w1 = item['weight']
+        >>> w2 = item.get('weight')
+        >>> w3 = item.weight
+        >>> assert(w1 == w2)
+        >>> assert(w2 == w3)
+        >>> assert(w1 == 10)
+        """
+        value = self.__dict__.get("properties", {}).get(key)
+        return value
+
+    def __setattr__(self, key, value):
+        """Sets properties dictionary values as if they
+        were properties on the class.
+
+        >>> item = Item("getattr test", weight=10)
+        >>> w1 = item['weight']
+        >>> w2 = item.get('weight')
+        >>> w3 = item.weight
+        >>> assert(w1 == w2)
+        >>> assert(w2 == w3)
+        >>> assert(w1 == 10)
+        >>> item.weight = 11
+        >>> assert(item.weight == 11)
+        """
+        if key in self.fields:
+            self.properties[key] = value
+        else:
+            super(Item, self).__setattr__(key, value)
+
+    def __getitem__(self, prop_name):
+        """Duplicates the getitem from a dictionary
+
+        Doctest:
+        >>> item = Item("setitem test", weight=10, materials=['leather', 'steel', 'iron'])
+        >>> print item['weight']
+        10
+        >>> item['weight'] = 11
+        >>> print item.weight
+        11
+        >>> item['materials'] = ['h20']
+        >>> print item['materials']
+        None
+        """
+        value = self.__dict__['properties'].get(prop_name)
+        return value
+
+    def __setitem__(self, prop_name, prop_value):
+        """Duplicates the setitem from a dictionary
+
+        Doctest:
+        >>> item = Item("setitem test", weight=10, materials=['leather', 'steel', 'iron'])
+        >>> print item.get('weight', 12)
+        10
+        >>> item['weight'] = 11
+        >>> print item.get('weight', 12)
+        11
+        >>> item['materials'] = ['h20']
+        >>> print item.get('materials', [])
+        []
+        """
+        if prop_name in self.fields:
+            self.properties[prop_name] = prop_value
 
     def __iter__(self):
-        for key in self.properties:
+        """Return in iterator which iterates through the properties
+
+        Doctest:
+        >>> item = Item("iter test", weight=10, materials=['leather', 'steel', 'iron'])
+        >>> for prop_name in item:
+        ...     print "{}".format(prop_name)
+        weight
+        """
+        iterator = self.properties
+        for key in iterator:
             yield key
 
     def iteritems(self):
-        for key, value in self.properties.iteritems():
+        """Returns an iterator over the key, value of the properties
+
+        Doctest:
+        >>> item = Item("iteritem test", weight=10, materials=['leather', 'steel', 'iron'])
+        >>> for prop_name, prop_value in item.iteritems():
+        ...     print "{}: {}".format(prop_name, prop_value)
+        weight: 10
+        """
+        iterator = self.properties.iteritems()
+        for key, value in iterator:
             yield key, value
 
     def __len__(self):
+        """Returns the number of properties in an item
+
+        Doctest:
+        >>> item = Item("len_test")
+        >>> len(item)
+        0
+        >>> item.properties['weight'] = 0
+        >>> len(item)
+        1
+        """
         return len(self.properties)
 
-    def get(self, name, default=None):
-        value = self.properties.get(name, default)
+    def get(self, prop_name, default_value=None):
+        """Returns property value if the prop_name is available or
+        returns the default value.
+
+        Doctest:
+        >>> item = Item("get test", materials=['leather', 'iron', 'steel'], weight=10)
+        >>> damage = item.get('damage')
+        >>> print damage
+        None
+        >>> materials = item.get('materials')
+        >>> print materials
+        None
+        >>> weight = item.get('weight')
+        >>> print weight
+        10
+        """
+        value = self.properties.get(prop_name, default_value)
         return value
 
-    def __repr__(self):
-        """Representation of item"""
-        string = "<{} {}:{} [{} {}]>"
-        cname = self.__class__.__name__
-        qty = self.quantity
-        if abs(qty) > 1:
-            qty = int(qty)
-        grams = "grams" if self.quantity > 1 else "gram"
-        health = self.health
-        if abs(health) > 1:
-            health = int(health)
-        return string.format(cname, self.name, health, qty, grams)
+    def __init__(self, name, **properties):
+        """Initializes Item"""
+        self.name = name
+        self.properties = {}
+        for prop_name, prop_value in properties.iteritems():
+            if prop_name in self.fields:
+                self.properties[prop_name] = prop_value
 
     def __add__(self, other):
-        item = self
+        """Adds Item to other"""
+        props = dict((k, v) for k, v in self.properties.iteritems())
+        new_item = Item(self.name, **props)
         if isinstance(other, Item):
-            properties = set(p for p in self)
-            properties.update(set(p for p in other))
-            new_props = {}
+            other_props = other.properties
+            properties = set(prop_name for prop_name in props)
+            properties.update(set(prop_name for prop_name in other_props))
             for prop_name in properties:
-                count = 0
-                if prop_name in self:
-                    count += 1
-                if prop_name in other:
-                    count += 1
-                val = self.get(prop_name, 0) + other.get(prop_name, 0)
-                val = val / 2.0
-                new_props[prop_name] = val
-            if new_props:
-                new_name = "_".join((self.name, other.name))
-                new_qty = sum([self.quantity, other.quantity])
-                item = Item(new_name, new_qty, new_props)
-        return item
+                # TODO:  Update for more types
+                prop_value = None
+                self_value = props.get(prop_name, prop_value)
+                other_value = other_props.get(prop_name, prop_value)
+                if not isinstance(self_value, type(other_value)):
+                    prop_value = self_value
+                    if self_value is None:
+                        prop_value = other_value
+                else:
+                    prop_value = self_value + other_value
+                props[prop_name] = prop_value
+            for prop_name in props:
+                new_item.properties[prop_name] = props[prop_name]
+        return new_item
+
+    def __repr__(self):
+        """Returns representation of the Item
+        Example:  <Item Longclaw [1450 grams]>
+        """
+        weight = self.get("weight", 0)
+        return "<Item {} [{} grams]>".format(self.name, weight)
 
 if __name__ == "__main__":
-    filepath = "item_data.yaml"
-    with open(filepath, 'r') as fd:
-        items = dict((name, Item(name=name, properties=prop))
-                     for name, prop in yaml_load(fd).iteritems())
+    import doctest
+    doctest.testmod()
 
-    new_item = items['coffee'] + items['vinegar']
-    print new_item
+    # i = Item("test")
+    # i.weight = 10
+    # print i.weight
