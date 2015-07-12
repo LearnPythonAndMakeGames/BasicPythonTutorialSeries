a9546dd5eeeb48bc3aba982440bc769087e40e58
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2015-07-12 13:08:22 -0500

    Added Food

---
+++ basic_tutorials/crafting_station/Food.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from Item import Item
+
+
+class Food(Item):
+    """Food Class
+
+    >>> from Item import yaml_load
+    >>> filepath = "food_item_data.yaml"
+    >>> foods = {}
+    >>> for food_name, food_data in yaml_load(filepath).iteritems():
+    ...    food = Food(food_name, weight=1, quantity=1, **food_data)
+    ...    foods[food_name] = food
+    >>> import pdb; pdb.set_trace()
+    """
+
+    field_filepath = "food_property_data.yaml"
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    # i = Item("test")
+    # i.weight = 10
+    # print i.weight
